59b9ac198f343c9b36d09f3897aca348f5bc92ba
githubankush <ankushkumayu04@gmail.com>
2025-05-30 15:46:41 +0530

    useGetOtherUsers hooks changes

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,27 +1,38 @@
-import  { useEffect } from 'react';
-import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 import { setOtherUsers } from '../redux/userSlice';
 import { BASE_URL } from '..';
 
 const useGetOtherUsers = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { token } = useSelector(state => state.user); // Assuming your token is saved here
 
-    useEffect(() => {
-        const fetchOtherUsers = async () => {
-            try {
-                axios.defaults.withCredentials = true;
-                const res = await axios.get(`${BASE_URL}/api/v1/user`);
-                // store
-                console.log("other users -> ",res);
-                dispatch(setOtherUsers(res.data));
-            } catch (error) {
-                console.log(error);
-            }
+  useEffect(() => {
+    const fetchOtherUsers = async () => {
+      try {
+        if (!token) {
+          console.warn('No auth token found');
+          return;
         }
-        fetchOtherUsers();
-    }, [])
 
-}
+        axios.defaults.withCredentials = true;
 
-export default useGetOtherUsers
+        const res = await axios.get(`${BASE_URL}/api/v1/user`, {
+          headers: {
+            Authorization: `Bearer ${token}`, // Pass token here
+          },
+        });
+
+        console.log('other users -> ', res.data);
+        dispatch(setOtherUsers(res.data));
+      } catch (error) {
+        console.error('Error fetching other users:', error);
+      }
+    };
+
+    fetchOtherUsers();
+  }, [dispatch, token]);
+};
+
+export default useGetOtherUsers;
